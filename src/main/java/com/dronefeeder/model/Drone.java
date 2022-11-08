dac26b8d40090e13a3dd35129c34c4c6b6a13526
Arthur Junior <arthurjr1906@gmail.com>
2022-11-08 03:41:59 -0300

    feat: Cria o model do drone

---
+++ src/main/java/com/dronefeeder/model/Drone.java
@@ -0,0 +1,76 @@
+package com.dronefeeder.model;
+
+import com.dronefeeder.domain.DroneStatusEnum;
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Drone {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Enumerated(EnumType.STRING)
+  private DroneStatusEnum status;
+
+  private LocalDateTime lastConnection;
+
+  private LocalDateTime created;
+
+  private double lon;
+
+  private double lat;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public DroneStatusEnum getStatus() {
+    return status;
+  }
+
+  public void setStatus(DroneStatusEnum status) {
+    this.status = status;
+  }
+
+  public LocalDateTime getLastConnection() {
+    return lastConnection;
+  }
+
+  public void setLastConnection(LocalDateTime lastConnection) {
+    this.lastConnection = lastConnection;
+  }
+
+  public double getLon() {
+    return lon;
+  }
+
+  public void setLon(double lon) {
+    this.lon = lon;
+  }
+
+  public double getLat() {
+    return lat;
+  }
+
+  public void setLat(double lat) {
+    this.lat = lat;
+  }
+
+  public LocalDateTime getCreated() {
+    return created;
+  }
+
+  public void setCreated(LocalDateTime created) {
+    this.created = created;
+  }
+}
