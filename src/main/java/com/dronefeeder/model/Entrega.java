0ed0d664c23fa3e4460dd8eb5093dabcbc6eac67
Arthur <Arthur@host.docker.internal>
2022-12-16 16:57:05 -0300

    Entrega model update

--- src/main/java/com/dronefeeder/model/Entrega.java
+++ src/main/java/com/dronefeeder/model/Entrega.java
@@ -1,13 +1,17 @@
 package com.dronefeeder.model;
 
-import com.dronefeeder.domain.EntregaStatusEnum;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import com.dronefeeder.domain.EntregaStatusEnum;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
  * Entrega Entity.
@@ -21,4 +25,52 @@ public class Entrega {
 
   @Enumerated(EnumType.STRING)
   private EntregaStatusEnum status;
+
+  private LocalDateTime created;
+
+  private LocalDateTime deliveryDate;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "droneId")
+  private Drone droneId;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public EntregaStatusEnum getStatus() {
+    return status;
+  }
+
+  public void setStatus(EntregaStatusEnum status) {
+    this.status = status;
+  }
+
+  public LocalDateTime getCreated() {
+    return created;
+  }
+
+  public void setCreated(LocalDateTime created) {
+    this.created = created;
+  }
+
+  public LocalDateTime getDeliveryDate() {
+    return deliveryDate;
+  }
+
+  public void setDeliveryDate(LocalDateTime deliveryDate) {
+    this.deliveryDate = deliveryDate;
+  }
+
+  public Drone getDroneId() {
+    return droneId;
+  }
+
+  public void setDroneId(Drone droneId) {
+    this.droneId = droneId;
+  }
 }
