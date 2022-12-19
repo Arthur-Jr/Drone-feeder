5dd971191d0f9bc1cda6587390bb854495f48074
Arthur <Arthur@host.docker.internal>
2022-12-19 17:22:40 -0300

    feat: Create entrega DTO

---
+++ src/main/java/com/dronefeeder/dto/EntregaDto.java
@@ -0,0 +1,13 @@
+package com.dronefeeder.dto;
+
+public class EntregaDto {
+  private String address;
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+}
