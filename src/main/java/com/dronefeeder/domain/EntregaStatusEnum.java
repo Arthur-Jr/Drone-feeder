2e2f650e7d1ee8bbe5023faa08b1f77c4eaf02c9
Arthur <Arthur@DESKTOP-NI5VI9I.mshome.net>
2022-12-19 23:29:51 -0300

    fix: enum update

--- src/main/java/com/dronefeeder/domain/EntregaStatusEnum.java
+++ src/main/java/com/dronefeeder/domain/EntregaStatusEnum.java
@@ -4,15 +4,5 @@ package com.dronefeeder.domain;
  * Entrega enum status.
  */
 public enum EntregaStatusEnum {
-  EP("em espera"), EV("em viagem"), ET("entregue");
-
-  private String value;
-
-  private EntregaStatusEnum(String value) {
-    this.value = value;
-  }
-
-  public String getValue() {
-    return value;
-  }
+  EM_ESPERA, EM_VIAGEM, ENTREGUE;
 }
