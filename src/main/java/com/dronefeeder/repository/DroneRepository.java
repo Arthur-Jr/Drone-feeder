f6cea502ea0253a8f704c20d4b3360918b1ba5bf
Arthur Jr <arthurjr1906@gmail.com>
2023-01-04 21:11:44 -0300

    fix: Remove @Repository

--- src/main/java/com/dronefeeder/repository/DroneRepository.java
+++ src/main/java/com/dronefeeder/repository/DroneRepository.java
@@ -1,13 +1,12 @@
 package com.dronefeeder.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.dronefeeder.model.Drone;
+
 
 /**
  * Drone Repo.
  */
-@Repository
 public interface DroneRepository extends JpaRepository<Drone, Long> {
 
 }
